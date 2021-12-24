e86c4749df9ab265091521e75d8fb96887cb0e3d
Aaron Froehlich <aaronfroehlich@gmail.com>
2021-12-24 07:37:49 -0500

    Add gh-pages script

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+drush tome:static -y -l https://a-fro.github.io/a-fro.com
+rm -rf gh-pages
+git clone git@github.com:a-fro/a-fro.com.git gh-pages
+cd gh-pages
+git checkout main || git checkout -b main
+cd ..
+rm -rf gh-pages/*
+cp -r html/a-fro.com/* gh-pages/
+cd gh-pages
+git add .
+git commit -m 'Updating gh-pages site'
+git push -u origin main
