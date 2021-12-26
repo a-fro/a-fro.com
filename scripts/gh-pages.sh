13e8a15db92c1475333d9129a8eead3c87a2437c
Aaron Froehlich <aaronfroehlich@gmail.com>
2021-12-26 05:24:34 -0500

    Update deployment script for custom domain

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -4,15 +4,16 @@ set -e
 
 mv web/sites/default/settings.local.php web/sites/default/settings.local-bk.php
 drush cr
-drush tome:static -y -l https://a-fro.github.io/a-fro.com
+drush tome:static -y -l https://www.a-fro.com
 rm -rf gh-pages
 git clone git@github.com:a-fro/a-fro.com.git gh-pages
 cd gh-pages
 git checkout main || git checkout -b main
 cd ..
 rm -rf gh-pages/*
-cp -r html/a-fro.com/* gh-pages/
+cp -r html/* gh-pages/
 cd gh-pages
+echo 'www.a-fro.com' > CNAME
 git add .
 git commit -m 'Updating gh-pages site'
 git push -u origin main
